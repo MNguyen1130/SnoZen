a056acf11eb56af9b1a13cdefe0a86c740ce6220
MNguyen1130 <matthewpnguyen@gmail.com>
2015-03-25 10:10:12 -0400

    Update
    Placeholdem - Fixed error with input type="email" where the message
    wasn't being displayed properly, as well as fixing the validation
    warning
    
    Contact - Class for email sending
    
    Bootstrap - Change a line that was causing a link coloring issue
    
    Freeze - Fixed up some css
    
    Home controller - Added in emailing functionality.. Kind of. Mostly a
    stub

--- SnoZen/Controllers/HomeController.cs
+++ SnoZen/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SnoZen.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,29 @@ namespace SnoZen.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Contact contact)
+        {
+            MailMessage mail = new MailMessage();
+    
+            // Insert your Smtp settings
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            smtpClient.Credentials = new System.Net.NetworkCredential("youremail", "password");
+            smtpClient.Port = 465;
+            smtpClient.EnableSsl = true;
+            smtpClient.Timeout = 100;
+
+            mail.From = new MailAddress(contact.Email);
+            mail.To.Add("youremail");
+            mail.Subject = "Inquiry From: " + contact.Name;
+            mail.Body = contact.Message;
+
+            // Uncomment when Smtp has been implemented
+            //smtpClient.Send(mail);
+
             return View();
         }
     }
